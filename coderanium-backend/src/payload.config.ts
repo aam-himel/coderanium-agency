950ad0e1d5a1f951f1105c0b98d4cb006c05b111
aam-himel <aam.himel@gmail.com>
2024-05-04 18:34:26 +0600

    initial commit

---
+++ coderanium-backend/src/payload.config.ts
@@ -0,0 +1,33 @@
+import path from 'path'
+
+import { payloadCloud } from '@payloadcms/plugin-cloud'
+import { mongooseAdapter } from '@payloadcms/db-mongodb'
+import { postgresAdapter } from '@payloadcms/db-postgres'
+import { webpackBundler } from '@payloadcms/bundler-webpack'
+import { slateEditor } from '@payloadcms/richtext-slate'
+import { buildConfig } from 'payload/config'
+
+import Users from './collections/Users'
+
+export default buildConfig({
+  admin: {
+    user: Users.slug,
+    bundler: webpackBundler(),
+  },
+  editor: slateEditor({}),
+  collections: [Users],
+  typescript: {
+    outputFile: path.resolve(__dirname, 'payload-types.ts'),
+  },
+  graphQL: {
+    schemaOutputFile: path.resolve(__dirname, 'generated-schema.graphql'),
+  },
+  plugins: [payloadCloud()],
+  db: postgresAdapter({
+    // Postgres-specific arguments go here.
+    // `pool` is required.
+    pool: {
+      connectionString: process.env.DATABASE_URI,
+    }
+  }),
+})
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Payload and Astro
+
+[Payload official site](https://payloadcms.com/)
+[Astro screwfast theme](https://astro.build/themes/details/screwfast/)
+
+- [ ] postgres local db
